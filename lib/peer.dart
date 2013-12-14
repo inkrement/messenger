702f8ffdc69271ee8874387bc0a43b8b950fe8da
Christian Hotz-Behofsits <chris.hotz.behofsits@gmail.com>
2013-12-14 09:19:15 +0100

    refactored

--- lib/peer.dart
+++ lib/peer.dart
@@ -1,5 +1,7 @@
 library peer;
 
+import 'dart:async';
+
 
 class Message{
   
@@ -8,6 +10,7 @@ class Message{
 
 abstract class Peer{
   List<Peer> _connections;
+  StreamController fetchDoneController = new StreamController.broadcast();
   
   connect(Peer other);
   
@@ -21,10 +24,14 @@ abstract class Peer{
   
   multicast() => broadcast(_connections);
   
-  //TODO: event
-  Message onreceive();
+  Stream get onReceive => fetchDoneController.stream;
+  
   close();
 }
+
+
+
+
 
 class MessagePassingPeer extends Peer{
   connect(MessagePassingPeer p){
@@ -35,6 +42,159 @@ class MessagePassingPeer extends Peer{
   send(MessagePassingPeer p){
     
   }
+  
+}
+
+
+//class JsWebRtcPeer extends Peer
+
+
+class WebRtcPeer extends Peer{
+  /*
+  RtcDataChannel bob_dc;
+   
+  RtcDataChannel alice_dc;
+  
+  var mediaConstraints = {
+                        /*  "optional": [],
+                          "mandatory": {
+                            "OfferToReceiveAudio": false, // Hmm!!
+                            "OfferToReceiveVideo": false // Hmm!!
+                          }*/
+  };
+  
+  log.info("start webrtc");
+  
+  RtcPeerConnection alice = new RtcPeerConnection(
+      {"iceServers": [{"url": "stun:stun.l.google.com:19302"}]},  
+        {"optional" : [{"RtpDataChannels": true}]}
+  );
+  
+  RtcPeerConnection bob = new RtcPeerConnection(
+      {"iceServers": [{"url": "stun:stun.l.google.com:19302"}]},  
+        {"optional" : [{"RtpDataChannels": true}]}
+  );
+  
+  alice.onIceConnectionStateChange.listen((event){
+    log.info("alice: iceConnectionStateChange");
+  });
+  
+  alice.onSignalingStateChange.listen((event){
+    log.info("alice: signalingChange event");
+  });
+  
+  bob.onIceConnectionStateChange.listen((event){
+    log.info("bob: iceConnectionStateChange");
+  });
+  
+  bob.onSignalingStateChange.listen((event){
+    log.info("bob: signalingChange event");
+  });
+  
+  
+  
+  /*alice.onNegotiationNeeded.listen((var data){
+    log.info("1# alice: onnegotiation needed");
+    
+    
+      
+    }); 
+  });*/
+
+  bob.onIceCandidate.listen((evt) {
+    if (evt.candidate)
+      print(evt.cancelable);
+  });
+  bob.onDataChannel.listen((var dc){
+    log.info("#3 bob got datachannel!");
+    
+    bob_dc = dc.channel;
+    
+    log.info(bob_dc.readyState);
+    
+    //bob_dc.onOpen.listen((var data){
+    //  log.info("bobs channel opened");
+      
+      bob_dc.onMessage.listen((var data){
+        log.info("bob: " + data);
+      });
+      
+      
+      
+      log.info("bob: send");
+      
+      log.info(dc.channel.negotiated.toString());
+      log.info(dc.channel.readyState);
+      dc.channel.sendString("hallo von bob");
+    //});
+    
+  });
+
+  alice.onIceCandidate.listen((evt) {
+    if(evt.candidate != null)
+      bob.addIceCandidate(evt.candidate, (){
+        log.info("seems to work");
+      }, (var error){
+        print(error.toString());
+        
+      });
+  });
+  
+  
+  log.info("alice: create dc");
+  
+  alice_dc = alice.createDataChannel("somelablel", {"reliable": false}); //fires onnegotiation needed
+  
+  log.info("alice set handler");
+  
+  alice_dc.onMessage.listen((var data){
+    log.info("alice: received " + data);
+    
+    //bouce message
+    alice_dc.sendString(data);
+  });
+  
+  
+  alice_dc.onOpen.listen((var data){
+    log.info("alice dc opened");
+    
+    alice_dc.sendString("test von alice");
+  });
+  
+  alice_dc.onClose.listen((var data){
+    log.info("alice dc closed");
+  });
+ 
+  
+  alice.createOffer(mediaConstraints).then((var sdp_alice){
+    log.info('2# alice: created offer');
+    //got offer
+    alice.setLocalDescription(sdp_alice);
+    bob.setRemoteDescription(sdp_alice);
+    
+    bob.createAnswer(mediaConstraints).then((RtcSessionDescription sdp_bob) {
+      log.info('3# bob: created answer');
+      bob.setLocalDescription(sdp_bob);
+      alice.setRemoteDescription(sdp_bob);
+      
+      log.info("desciptions set");
+      
+      bob_dc.sendString("tesst");
+      alice_dc.sendString("rest");
+      
+      
+    });
+    
+  });
+   */
+}
+
+class Chart{
+  
+}
+
+class GroupChat{
+  
 }
 
 
