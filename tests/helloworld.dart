e32af6e267faea823a880d70cace21a0c1cd7df8
Christian Hotz-Behofsits <chris.hotz.behofsits@gmail.com>
2013-12-14 15:18:35 +0100

    first unittest added

---
+++ tests/helloworld.dart
@@ -0,0 +1,11 @@
+
+
+import 'package:unittest/unittest.dart';
+
+void main() {
+  test('SendAndReceive', (){
+    String hello = "hello world";
+    
+    expect(hello, "hello world");
+  });
+}
---
+++ tests/messagepassingpeer.dart
@@ -0,0 +1,15 @@
+import 'package:unittest/unittest.dart';
+import 'package:webrtc/Messenger/messenger.dart';
+
+void main() {
+  test('SendAndReceive', (){
+    MessagePassingPeer alice = new MessagePassingPeer();
+    MessagePassingPeer bob = new MessagePassingPeer();
+    
+    bob.onReceive.listen((NewMessageEvent data){
+      expect(data.getMessage(), "something new");
+    });
+    
+    alice.sendString(bob, "something new");
+  });
+}
