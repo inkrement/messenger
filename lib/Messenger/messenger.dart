e27349d96644df9830c9f3e16070639987ee656a
Christian Hotz-Behofsits <chris.hotz.behofsits@gmail.com>
2014-01-01 11:25:07 +0100

    refactored library structure

--- lib/Messenger/messenger.dart
+++ lib/Messenger/messenger.dart
@@ -2,36 +2,35 @@ library messenger;
 
 //TODO: load all
 
-import 'dart:async';
-//import 'dart:js';
-import 'dart:html';
+//import 'package:logging/logging.dart';
 
 
-import 'package:logging/logging.dart';
+import 'src/components/peer.dart';
 
-/*
- * own libs
+/**
+ * test environment and return best peer
  */
-import 'src/events.dart';
-export 'src/events.dart';
 
-import 'src/components/message.dart';
-export 'src/components/message.dart';
-
-import 'src/components/signaling.dart';
-export 'src/components/signaling.dart';
-
-
-/*
-part 'src/peers/peer.dart';
-part 'src/peers/messagepassingpeer.dart';
-part 'src/peers/webrtcpeer.dart';
-part 'src/peers/jswebrtcpeer.dart';
-
-//part 'src/peers/connections/connection.dart';
-//part 'src/peers/connections/jswebrtcconnection.dart';
-
-part 'src/peers/connections/ReadyStates.dart';
-part 'src/peers/connections/ReadyStates.dart';
-
-*/
+class Messenger{
+  final Peer _peer;
+  
+  const Messenger._create(this._peer);
+  
+  factory Messenger(){
+    if(_peer == null){
+      switch(testEnv()){
+        case "":
+        case "":
+        case "":
+        
+      }
+      //TODO: test
+      return new Messenger._create(new JsWebRtcPeer());
+    }
+  }
+  
+  static String testEnv(){
+    
+  }
+  
+}
